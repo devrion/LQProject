22ed69f2d0c6083b028158ab027740ee86d5414c
Devrion Wells <devrionwells2012@gmail.com>
2018-11-14 20:18:34 -0500

    finalphase

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 const element = document.querySelector(".nav-item");
-var share = document.querySelector("#share-fb")
+var share = document.querySelectorAll(".sharer")
 
 element.addEventListener("mouseover", event => {
   console.log("Mouse over");
@@ -10,7 +10,7 @@ element.addEventListener("mouseout", event => {
   console.log("Mouse out");
 });
 
-
-share.addEventListener("click", function(){
-  share.setAttribute('target', '_blank');
-})
+for (var i = 0; i < share.length; i++){
+  var j = i;
+  share[j].setAttribute('target', 'blank');
+}
